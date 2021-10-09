6cfd9faefae7317488314aff3b8fcc3c13cf9de7
Bello Afeez <belloafeez7@gmail.com>
2021-10-09 12:36:44 +0800

    done

--- functions/hello.js
+++ functions/hello.js
@@ -1,9 +1,30 @@
+let voteArr = [0,0,0,0];
+
+
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
+  const v = event.queryStringParameters.vote
+
+    if (v === "a") {
+      voteArr[0] += 1
+    }else if (v === "b") {
+      voteArr[1] += 1
+    }else if (v === "c") {
+      voteArr[2] += 1
+    }else if (v === "d") {
+      voteArr[3] += 1
+    }else if (v === "r") {
+      voteArr = [0,0,0,0]
+    }
+
+    // return JSON.stringify(voteArr);
+
+
+  // const subject = event.queryStringParameters.name || 'World'
 
   return{
     statusCode: 200,
-    body: `Hello ${subject}`
+    headers: {"Access-Control-Allow-Origin" : "*"},
+    body: JSON.stringify(voteArr),
   }
 
 }
